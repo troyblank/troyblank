14820aafc9eb822168e568142c45fe795cd8e89c
troyblank <troy@troyblank.com>
2016-01-16 13:03:00 -0600

    out with the rest of python

--- contact/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-
-class ContactForm(forms.Form):
-	name = forms.CharField(max_length=100)
-	email = forms.EmailField(max_length=100)
-	message = forms.CharField(max_length=500, widget=forms.Textarea)
